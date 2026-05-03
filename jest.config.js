a73632c9274db0aec1d09c1146b240dea4992097
KoviSomaMester <113170219+KoviSomaMester@users.noreply.github.com>
2026-05-03 20:40:11 +0200

    Add Jest configuration for testing environment

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js'],
+  collectCoverageFrom: [
+    'routes/**/*.js',
+    'models/**/*.js',
+    '!node_modules/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
+  testTimeout: 30000,
+  detectOpenHandles: true,
+};
